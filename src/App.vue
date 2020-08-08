f5bc6000a83b8b313e8ac16a2288a01e7eed3f22
Alancx <Gol2illa.ome@gmail.com>
2020-08-08 14:05:14 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-app>
+   
+    <v-app-bar app color="primary" dark clipped-left>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+     <v-card>
+      <v-navigation-drawer
+        v-model="drawer"
+        :mini-variant.sync="mini"
+        permanent
+        app
+        clipped
+      >
+        <v-list dense>
+          <v-list-item v-for="item in items" :key="item.title" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title> <router-link :to="item.to">{{ item.title }} </router-link></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        
+          <v-divider></v-divider>
+         <v-list-item class="px-2">
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+      </v-navigation-drawer>
+    </v-card>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+  data: () => ({
+    //
+    drawer: true,
+    items: [
+      { title: "Home", icon: "mdi-home-city",to: "/" },
+      { title: "About", icon: "mdi-account",to: "/about" },
+      { title: "Users", icon: "mdi-account-group-outline" },
+    ],
+    mini: true,
+  }),
+};
+</script>
