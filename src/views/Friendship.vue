2b224cbf6c3398f595c24b14aa1b5c6b9a959827
tanupat <tanupat.boonruksa@gmail.com>
2020-08-08 19:11:53 +0700

    menu multi

---
+++ src/views/Friendship.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Friendship">
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12">
+          <h1>Friendship</h1>
+          <v-divider></v-divider>
+          <v-responsive :aspect-ratio="16/9">
+            <v-card-text>Your interactions with villagers, Koropokkur, pet animals, wild animals, and livestock can change their likability points (relationship points / friendship points) towards you. This likability / relationship / friendship points manifest in their likability degree (how many hearts they have when you talk to them).</v-card-text>
+          </v-responsive>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="12">
+          <h3>Likability degree</h3>
+          <v-divider></v-divider>
+          <v-responsive :aspect-ratio="16/9">
+            <v-card-text>Your interactions with villagers, Koropokkur, pet animals, wild animals, and livestock can change their likability points (relationship points / friendship points) towards you. This likability / relationship / friendship points manifest in their likability degree (how many hearts they have when you talk to them).</v-card-text>
+          </v-responsive>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Friendship",
+  components: {}
+};
+</script>
